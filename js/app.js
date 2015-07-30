c1c6c4a6e68e775dd47129e6d26eff762ffe1e87
unknown <schacht.german@gmail.com>
2015-07-29 22:56:18 -0300

    added login and so so

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-Parse.initialize("SurJUfsG3JkChlpYg6BIW9dMwvl8wgDcEZhPJhXR", "AeiH3QH3vfHbaQ4IUoXXe3FFInd5XIiuquHiXr3S");
+// Inicializar el parse
+Parse.initialize('SurJUfsG3JkChlpYg6BIW9dMwvl8wgDcEZhPJhXR', 'AeiH3QH3vfHbaQ4IUoXXe3FFInd5XIiuquHiXr3S');
+
+// Inicializar variables
+// Usuario actual
+var currentUser;
+
+// Evento submit
+$('#login-form').submit(function () {
+ login();
+ return false;
+});
+
+$('#salir').click(function () {
+  logout();
+})
+
+// Preguntar si hay usuario logeado o no
+showLoginOrBody();
+
+function login() {
+  var usuario = $('#username').val();
+  var contrasena = $('#password').val();
+
+  console.log(usuario+'/'+contrasena);
+
+  Parse.User.logIn(usuario, contrasena, {
+    success: function(user) {
+      console.log('login-success');
+      showLoginOrBody();
+    },
+    error: function(user, error) {
+      console.log('login-error');
+      alert('Error en inicio de sesión.');
+    }
+  });
+}
+
+function logout() {
+  Parse.User.logOut();
+  showLoginOrBody();
+}
+
+function showLoginOrBody() {
+  currentUser = Parse.User.current();
+  if (currentUser) {
+    $('.parse-body').show();
+    $('.parse-login').hide();
+  } else {
+    $('.parse-login').show();
+    $('.parse-body').hide();
+  }
+}
