7650213f8fe289c36a4347551a9aea95ff966550
unknown <schacht.german@gmail.com>
2015-07-29 23:12:42 -0300

    Added signup

--- js/app.js
+++ js/app.js
@@ -11,6 +11,11 @@ $('#login-form').submit(function () {
  return false;
 });
 
+$('#signup-form').submit(function () {
+ signup();
+ return false;
+});
+
 $('#salir').click(function () {
   logout();
 })
@@ -19,19 +24,44 @@ $('#salir').click(function () {
 showLoginOrBody();
 
 function login() {
-  var usuario = $('#username').val();
-  var contrasena = $('#password').val();
-
-  console.log(usuario+'/'+contrasena);
+  var usuario = $('#login-username').val();
+  var contrasena = $('#login-password').val();
 
   Parse.User.logIn(usuario, contrasena, {
     success: function(user) {
       console.log('login-success');
       showLoginOrBody();
+      $('#login-username').val('');
+      $('#login-password').val('');
     },
     error: function(user, error) {
       console.log('login-error');
       alert('Error en inicio de sesión.');
+    }
+  });
+}
+
+function signup() {
+  var usuario = $('#signup-username').val();
+  var contrasena = $('#signup-password').val();
+  var email = $('#signup-email').val();
+
+  var user = new Parse.User();
+  user.set("username", usuario);
+  user.set("password", contrasena);
+  user.set("email", email);
+
+  user.signUp(null, {
+    success: function(user) {
+      console.log('signup-success');
+      showLoginOrBody();
+      $('#signup-username').val('');
+      $('#signup-password').val('');
+      $('#signup-email').val('');
+    },
+    error: function(user, error) {
+      console.log('signup-error');
+      alert("Error en registro");
     }
   });
 }
