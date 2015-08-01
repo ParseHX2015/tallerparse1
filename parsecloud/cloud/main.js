3727e4732b9313a4cb91db5b27c7f5556a0118ea
ramirezcgn <ramirezcgn@gmail.com>
2015-07-31 23:43:51 -0300

    fix username check

--- parsecloud/cloud/main.js
+++ parsecloud/cloud/main.js
@@ -15,7 +15,7 @@ Parse.Cloud.beforeSave(Parse.User, function (request, response) {
     var username = usuario.get('username').trim();
     var email = usuario.get('email').trim();
    
-    if (username.length < 4 || username.length > 16 || checkName.test(username)) {
+    if (username.length < 4 || username.length > 16 || !checkName.test(username)) {
       response.error('El nombre de usuario debe tener entre 4 y 16 caracteres y no contener caracteres especiales');
     }
     else if (!emailFormat.test(email)) {
