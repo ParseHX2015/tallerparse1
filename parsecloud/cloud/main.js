b41543d5986e77e28d50e63b6b477c4e1a8a7506
ramirezcgn <ramirezcgn@gmail.com>
2015-07-31 23:11:15 -0300

    Fixed data edition

--- parsecloud/cloud/main.js
+++ parsecloud/cloud/main.js
@@ -8,18 +8,23 @@ var helpers = require('cloud/helpers.js');
 Parse.Cloud.beforeSave(Parse.User, function (request, response) {
   var checkName = /^[a-zA-Z0-9_-]+$/;
   var emailFormat = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
- 
   var usuario = request.object;
-  var username = usuario.get('username').trim();
-  var email = usuario.get('email').trim();
- 
-  if (username.length < 4 || username.length > 16 || checkName.test(username)) {
-    response.error('El nombre de usuario debe tener entre 4 y 16 caracteres y no contener caracteres especiales');
-  }
-  else if (!emailFormat.test(email)) {
-    response.error('Formato de email incorrecto');
-  }
-  else {
+
+  //nuevo usuario
+  if(!usuario.id){
+    var username = usuario.get('username').trim();
+    var email = usuario.get('email').trim();
+   
+    if (username.length < 4 || username.length > 16 || checkName.test(username)) {
+      response.error('El nombre de usuario debe tener entre 4 y 16 caracteres y no contener caracteres especiales');
+    }
+    else if (!emailFormat.test(email)) {
+      response.error('Formato de email incorrecto');
+    }
+    else {
+      response.success();
+    }
+  } else {
     response.success();
   }
 });
@@ -27,28 +32,34 @@ Parse.Cloud.beforeSave(Parse.User, function (request, response) {
 // beforeSave: Data validation
 Parse.Cloud.beforeSave('Evento', function (request, response) {
   var evento = request.object;
-  var nombre = evento.get('Nombre').trim();
-  var disertantes = evento.get('Disertantes').trim();
-  var descripcion = evento.get('Descripcion').trim();
-  var fecha = evento.get('Fecha');
-  var duracion = evento.get('Duracion');
 
-  if (nombre.length < 3) {
-    response.error('El nombre de evento debe tener más de 3 caracteres');
-  }
-  else if (disertantes.length < 3) {
-    response.error('Debe definir los disertantes del evento');
-  }
-  else if (!fecha) {
-    response.error('La fecha del evento debe estar definida');
-  }
-  else if (!duracion) {
-    response.error('La duración del evento debe estar definida');
-  }
-  else if (descripcion.length < 4) {
-    response.error('La descripcion del evento debe tener más de 4 caracteres');
-  }
-  else {
+  //nuevo evento
+  if(!evento.id){
+    var nombre = evento.get('Nombre').trim();
+    var disertantes = evento.get('Disertantes').trim();
+    var descripcion = evento.get('Descripcion').trim();
+    var fecha = evento.get('Fecha');
+    var duracion = evento.get('Duracion');
+
+    if (nombre.length < 3) {
+      response.error('El nombre de evento debe tener más de 3 caracteres');
+    }
+    else if (disertantes.length < 3) {
+      response.error('Debe definir los disertantes del evento');
+    }
+    else if (!fecha) {
+      response.error('La fecha del evento debe estar definida');
+    }
+    else if (!duracion) {
+      response.error('La duración del evento debe estar definida');
+    }
+    else if (descripcion.length < 4) {
+      response.error('La descripcion del evento debe tener más de 4 caracteres');
+    }
+    else {
+      response.success();
+    }
+  } else {
     response.success();
   }
 });
